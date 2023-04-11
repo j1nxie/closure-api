90c5a0bd78f299db44dd87fb6bd7d96dae31f8d3
j1nxie <rylieeeeexd@gmail.com>
2023-04-12 00:29:28 +0700

    feat: return current event name + banner image

---
+++ src/getEvent.ts
@@ -0,0 +1,58 @@
+import { JSDOM } from "jsdom";
+import fetch from "node-fetch";
+
+const BASE_URL = "https://gamepress.gg/arknights/";
+
+async function getEvent() {
+	try {
+		const html = await fetch(BASE_URL, {
+			headers: {
+				accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+				"accept-language": "en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7",
+				"cache-control": "max-age=0",
+				"sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": '"Windows"',
+				"sec-fetch-dest": "document",
+				"sec-fetch-mode": "navigate",
+				"sec-fetch-site": "none",
+				"sec-fetch-user": "?1",
+				"upgrade-insecure-requests": "1",
+				cookie: "SSESSf1fb0a9c0c1f97d9ff71d05ced8d6063=ArpkhEC7pPUGTOc-5nRrTmOKu3efHuTwEtBUJs2wblY",
+			},
+			referrerPolicy: "strict-origin-when-cross-origin",
+			body: null,
+			method: "GET",
+		});
+		const text = await html.text();
+		const dom = new JSDOM(text);
+
+		const document = dom.window.document;
+
+		/* eslint-disable @typescript-eslint/no-non-null-assertion */
+		const eventList = document.querySelector(
+			".field.field--name-field-page-content.field--type-entity-reference-revisions.field--label-hidden.field__items"
+		)!.children[0];
+
+		const imageElements = eventList.getElementsByTagName("img");
+		let event;
+
+		if (imageElements.length === 2) {
+			event = imageElements[1];
+		} else {
+			event = imageElements[0];
+		}
+
+		const eventName = event.alt;
+		const eventBanner = event.src;
+
+		return {
+			eventName,
+			eventBanner,
+		};
+	} catch (err) {
+		return `error: ${err}`;
+	}
+}
+
+export default getEvent;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World")
+import eventRouter from "./eventRoute.js";
+import cors from "@fastify/cors";
+import fastify from "fastify";
+
+const app = fastify({
+	ignoreTrailingSlash: true,
+});
+
+void app.register(eventRouter, { prefix: "/api" });
+
+async function start() {
+	try {
+		await app.register(cors);
+		await app.listen({ port: 4000, host: "192.168.243.128" });
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+}
+
+void start();
