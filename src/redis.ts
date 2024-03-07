a43a6bd4c41f3eb04c61d6c6e94077c8cd3acb56
j1nxie <rylieeeeexd@gmail.com>
2024-03-07 21:11:12 +0700

    fix: allow setting redis url and port

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,9 @@
 import { createClient } from "redis";
+import * as dotenv from "dotenv";
 
-const client = await createClient()
+dotenv.config();
+
+const client = await createClient({ url: `redis://${process.env.REDIS_URL ?? "localhost"}:${process.env.REDIS_PORT ?? "6379"}` })
 	.on("error", (err) => {
 		// eslint-disable-next-line no-console
 		console.error("redis error:", err);
